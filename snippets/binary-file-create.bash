2d5c1bff4661f7aea3a89325fca3e8c57e6c5007
sammtcbn <sammtcbn@gmail.com>
2023-05-12 14:05:17 +0800

    add sample using head and urandom device to generate binary

--- snippets/binary-file-create.bash
+++ snippets/binary-file-create.bash
@@ -8,3 +8,9 @@ tr '\000' '\377' < /dev/zero | dd of=image_128M_FF.bin bs=1K count=131072
 
 # 64 bytes , fill random value
 dd if=/dev/urandom of=image_64B_rand.bin bs=1 count=64
+
+# 1M , fill random value , using head + /dev/urandom
+head -c 1M /dev/urandom > image_1MB_rand.bin
+
+# 1G , fill random value , using head + /dev/urandom
+#head -c 1G /dev/urandom > image_1GB_rand.bin
