261c8ac367549aeaae19f92ac28804ecdf5bf68d
sammtcbn <sammtcbn@gmail.com>
2023-06-07 10:44:37 +0800

    elapsed_time_ex1 update

--- snippets/elapsed_time_ex1.bash
+++ snippets/elapsed_time_ex1.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 # refer to https://www.xmodulo.com/measure-elapsed-time-bash.html
 start_time=$(date +%s)
-date '+%Y-%m-%d %H:%M:%S' -d @$start_time
-
+start_time_str=$(date '+%Y-%m-%d %H:%M:%S' -d @$start_time)
+echo start_time_str is ${start_time_str}
 sleep 6
 
 end_time=$(date +%s)
-date '+%Y-%m-%d %H:%M:%S' -d @$end_time
+end_time_str=$(date '+%Y-%m-%d %H:%M:%S' -d @$end_time)
+echo end_time_str is ${end_time_str}
 
 elapsed=$(( end_time - start_time ))
 
