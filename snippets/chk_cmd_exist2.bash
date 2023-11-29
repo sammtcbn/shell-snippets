7b58f6cd7ab6bc8d89d84cdc073c3e1c1528a109
sammtcbn <sammtcbn@gmail.com>
2023-11-29 12:07:29 +0800

    minor update

--- snippets/chk_cmd_exist2.bash
+++ snippets/chk_cmd_exist2.bash
@@ -4,3 +4,5 @@ if command -v mosquitto_pub &> /dev/null; then
 else
     echo mosquitto_pub not found
 fi
+
+command -v pdftk >/dev/null 2>&1 || { echo >&2 "pdftk is required but not installed. Aborting."; exit 1; }
